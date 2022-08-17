7a2b71f0642e0076f9cb561ace1df7d2b01abd8d
David Tavadze <tavadzed@gmail.com>
2022-08-17 14:27:03 +0400

    for vercel

--- pages/info/pools/[poolsId]/index.js
+++ pages/info/pools/[poolsId]/index.js
@@ -80,7 +80,7 @@ export async function getStaticPaths(context) {
 
 export async function getStaticProps(context) {
   const client = await MongoClient.connect(
-    'mongodb+srv://sokrat:sokrat12345@cluster0.x2cvw.mongodb.net/cmcx?retryWrites=true&w=majority',
+    'mongodb+srv://sokrat:lalala12345@cluster0.x2cvw.mongodb.net/cmcx?retryWrites=true&w=majority',
   );
   const db = client.db();
 
--- pages/nfts/collections/[buyId]/index.js
+++ pages/nfts/collections/[buyId]/index.js
@@ -287,7 +287,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const client = await MongoClient.connect(
-    'mongodb+srv://sokrat:sokrat12345@cluster0.x2cvw.mongodb.net/cmcx?retryWrites=true&w=majority',
+    'mongodb+srv://sokrat:lalala12345@cluster0.x2cvw.mongodb.net/cmcx?retryWrites=true&w=majority',
   );
   const db = client.db();
 
