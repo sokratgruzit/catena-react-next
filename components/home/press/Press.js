86cf367ee5ab003ecabd4f691aad7a81ad0e4f78
salo <chkhartishvili202@gmail.com>
2023-05-19 12:52:51 +0400

    update

--- components/home/press/Press.js
+++ components/home/press/Press.js
@@ -3,12 +3,12 @@ import Title from "../components/Title";
 import JoinCommunity from "../components/JoinCommunity";
 
 const Press = () => {
-    return(
-     <div className="container" style={{ paddingTop: "100px" }}>
-        <Title spanOne="core" spanTwo="press"/>
-        <JoinCommunity />
-     </div>
-    )
-}
+  return (
+    <div className="container" style={{ paddingTop: "100px" }}>
+      <Title spanOne="core" spanTwo="press" />
+      <JoinCommunity />
+    </div>
+  );
+};
 
 export default Press;
