daa3a40a2cce4e3f9b026821990573bd67f20d04
NTsloth <sloth2003nt@gmail.com>
2023-05-24 17:28:56 +0400

    roadmap page

---
+++ pages/home/roadmap/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Roadmap from "../../../components/home/roadmap/Roadmap";
+
+function index() {
+  return (
+    <div>
+      <Roadmap />
+    </div>
+  );
+}
+
+export default index;
