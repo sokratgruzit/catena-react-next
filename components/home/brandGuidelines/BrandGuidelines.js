ff28a4763dedadef78886c915ffb2404b13297fb
NTsloth <sloth2003nt@gmail.com>
2023-06-01 15:37:41 +0400

    guidlines

--- components/home/brandGuidelines/BrandGuidelines.js
+++ components/home/brandGuidelines/BrandGuidelines.js
@@ -1,17 +1,16 @@
-import ClearSpace from "./components/ClearSpace";
-import ColorBanner from "./components/ColorBanner";
-import ColorPalette from "./components/ColorPalette";
-import Colors from "./components/Colors";
-import IntroBanner from "./components/IntroBanner";
-import LogoUsages from "./components/LogoUsages";
-import LogoVariations from "./components/LogoVariations";
-import LogosBanner from "./components/LogosBanner";
-import MainColors from "./components/MainColors";
-import Partniership from "./components/Partniership";
-import ProductLogos from "./components/ProductLogos";
-import TechnologiesBanner from "./components/TechnologiesBanner";
-import Typography from "./components/Typography";
-
+import ClearSpace from './components/ClearSpace';
+import ColorBanner from './components/ColorBanner';
+import ColorPalette from './components/ColorPalette';
+import Colors from './components/Colors';
+import IntroBanner from './components/IntroBanner';
+import LogosBanner from './components/LogosBanner';
+import LogoUsages from './components/LogoUsages';
+import LogoVariations from './components/LogoVariations';
+import MainColors from './components/MainColors';
+import Partniership from './components/Partniership';
+import ProductLogos from './components/ProductLogos';
+import TechnologiesBanner from './components/TechnologiesBanner';
+import Typography from './components/Typography';
 
 function BrandGuidelines() {
   return (
--- components/home/brandGuidelines/components/Colors.js
+++ components/home/brandGuidelines/components/Colors.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Colors() {
-  const colorsList = ["CORE Red", "Blue", "White", "Black"];
+  const colorsList = ['CORE Red', 'Blue', 'White', 'Black'];
   const displayedColors = [];
 
   return (
@@ -13,29 +13,38 @@ export default function Colors() {
 
           return (
             <div key={index}>
-              <div data-aos="fade-up" className="pT-160 guidline__flex">
-                <div className="guidline__bg-outer"></div>
-                <div className="guidline__half">
-                  <div className="guidline__lrg-ttl font-51">{item}</div>
+              <div data-aos='fade-up' className='pT-160 guidline__flex'>
+                <div className='guidline__bg-outer'></div>
+                <div className='guidline__half'>
+                  <div className='guidline__lrg-ttl font-51'>{item}</div>
                 </div>
-                <div className="guidline__half">
-                  <div className="guidline__paragraph paragraphMt">
-                    {/* Add your color description here */}
-                  </div>
+                <div className='guidline__half'>
+                  <div className='guidline__paragraph paragraphMt'>{/* Add your color description here */}</div>
                 </div>
               </div>
-              <div data-aos="fade-up" className="guidline__color-scheme">
-                <div className="guidline__color-scheme-item" style={{ background: "#FF7152" }}>
-                  <div className="guidline__color-scheme-item-ttl">{item}</div>
-                  <div className="guidline__colors-inner-description-floor-descr"><span className="wdth1">FF7152</span></div>
-                  <div className="guidline__colors-inner-description-floor-descr" style={{ marginRight: "30px" }}><span className="wdth2">R=255</span><span className="wdth2">G=113</span><span className="wdth2">B=82</span></div>
-                  <div className="guidline__colors-inner-description-floor-descr"><span className="wdth3">C=1</span><span className="wdth3">M=69</span><span className="wdth3">Y=74</span><span className="wdth3">K=0</span></div>
+              <div data-aos='fade-up' className='guidline__color-scheme'>
+                <div className='guidline__color-scheme-item' style={{ background: '#FF7152' }}>
+                  <div className='guidline__color-scheme-item-ttl'>{item}</div>
+                  <div className='guidline__colors-inner-description-floor-descr'>
+                    <span className='wdth1'>FF7152</span>
+                  </div>
+                  <div className='guidline__colors-inner-description-floor-descr' style={{ marginRight: '30px' }}>
+                    <span className='wdth2'>R=255</span>
+                    <span className='wdth2'>G=113</span>
+                    <span className='wdth2'>B=82</span>
+                  </div>
+                  <div className='guidline__colors-inner-description-floor-descr'>
+                    <span className='wdth3'>C=1</span>
+                    <span className='wdth3'>M=69</span>
+                    <span className='wdth3'>Y=74</span>
+                    <span className='wdth3'>K=0</span>
+                  </div>
                 </div>
               </div>
             </div>
           );
         } else {
-          return null; 
+          return null;
         }
       })}
     </div>
