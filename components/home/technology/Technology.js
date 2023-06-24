646135040258bc14d0caa31fafaef1cc2ea78b4a
NTsloth <sloth2003nt@gmail.com>
2023-06-24 13:57:18 +0400

    merge

--- components/home/technology/Technology.js
+++ components/home/technology/Technology.js
@@ -1,5 +1,4 @@
 import Cart from './components/Cart';
-import ConsensusStrucure from './components/ConsensusStrucure';
 import Features from './components/Features';
 import Layer from './components/Layer';
 import Promo from './components/Promo';
@@ -45,51 +44,15 @@ export default function Technology() {
         'To bring it all together, we use Rust, Haskell, C++,  and Golang to program everything. These programming languages are the most advanced in the market and provide high speeds and robust performance. All of this combined makes the Apeirogon framework a high-speed, and robust solution.',
     },
   ];
-  const cartItems = [
-    {
-      image: 'Pivot-Chain.jpg',
-      title: 'Pivot Chain',
-      description:
-        '  This is the foundation of the CORE platform. It controls the whole network and is responsible for its security, consensus, and monitoring.',
-    },
-    {
-      image: 'PolyChains.jpg',
-      title: 'PolyChains',
-      description:
-        ' Multiple chains that run simultaneously, that either form instances of an existing blockchain or are independent blockchains with their own use cases and tokens Description for Item 2',
-    },
-    {
-      image: 'Bridges.jpg',
-      title: 'Bridges',
-      description:
-        ' Allow Polychains, both private and public, to connect with other public blockchains like Bitcoin and Ethereum, without the use of oracles',
-    },
-  ];
-  const layer1Images = ['image1-1.jpg', 'image1-2.jpg', 'image1-3.jpg', 'image1-4.jpg'];
 
-  const layer2Images = ['image2-1.jpg', 'image2-2.jpg', 'image2-3.jpg', 'image2-4.jpg'];
-
-  const layer3Images = ['image3.jpg'];
-  const layer4Images = ['image4.jpg'];
-  const tokentArray = [
-    {
-      title: 'The CMCX Token',
-      description:
-        'The CMCX token is native to the CORE platform and allows users to transact with other users on the blockchain, as well as pay for all of the utility fees in the ecosystem. It can be used for governance, staking, transactions, smart contracts and validator rewards on the platform.',
-    },
-  ];
   return (
     <div className='container' style={{ paddingTop: '100px' }}>
       <Promo />
       <Tabs tabs={tabs} />
-      <Cart items={cartItems} />
-      <Layer title='Application' description='Aura' images={layer1Images} />
-      <Layer title='Blockchain' description='Catena' images={layer2Images} />
-      <hr />
-      <Layer title='Network' description='Origo' images={layer3Images} />
-      <Layer title='Hardware' description='Layer -1' images={layer4Images} />
-      <Token content={tokentArray} />
-      <ConsensusStrucure />
+      <Cart titpeSpanOne={'Network'} titpeSpanTwo={'Structure'} />
+      <Layer />
+      <Token />
+      <Cart titpeSpanOne={'Consesus'} titpeSpanTwo={'Structure'} />
       <Features />
     </div>
   );
