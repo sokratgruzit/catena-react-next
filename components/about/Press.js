b9aa64b921f0211c6fde90b221f51f61b4ab83e6
entost <tsotnentost@gmail.com>
2023-07-19 17:33:20 +0400

    last

--- components/about/Press.js
+++
@@ -1,7 +0,0 @@
-import styles from './Press.module.css';
-
-const Press = () => {
-  return <div className={styles.container}></div>;
-};
-
-export default Press;
--- pages/about/[press].js
+++ pages/about/[press].js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import Press from '../../components/about/Press';
+import Press from '../../components/home/press/Press';
 
 const Index = () => {
   const router = useRouter();
