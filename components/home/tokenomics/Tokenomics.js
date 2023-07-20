e73564787e04b48a7bae84b40c47ccd3ac75e8b3
NTsloth <sloth2003nt@gmail.com>
2023-07-20 23:01:47 +0400

    guidelines

--- components/home/tokenomics/Tokenomics.js
+++ components/home/tokenomics/Tokenomics.js
@@ -1,19 +1,13 @@
-import Certik from './components/Certik';
-import Distribution from './components/Distribution';
-import Download from './components/Download';
-import Schedule from './components/Schedule';
-import Title from './components/Title';
 import TokenomicsHead from './components/TokenomicsHead';
 
 function Tokenomics() {
   return (
     <div className='container'>
-      <Title />
       <TokenomicsHead />
-      <Distribution />
+      {/* <Distribution />
       <Schedule />
       <Certik />
-      <Download />
+      <Download /> */}
     </div>
   );
 }
