f77577ca6eb5a54cdda33f0e7d5f1849ba145f65
NTsloth <sloth2003nt@gmail.com>
2023-07-21 14:46:03 +0400

    download button

--- components/home/brandGuidelines/components/IntroBanner.js
+++ components/home/brandGuidelines/components/IntroBanner.js
@@ -1,3 +1,5 @@
+import Download from './Download';
+
 import styles from '../styles/IntroBanner.module.css';
 
 export default function IntroBanner() {
@@ -21,9 +23,11 @@ export default function IntroBanner() {
                 <span>{item.guidelines}</span>
               </h1>
               <div className={styles.mainContainerDownload}>
-                <a href='/resources/core-logo-pack.zip' target='_blank' className={styles.mainContainerDownloadBtn}>
+                {/* <a href='/resources/core-logo-pack.zip' target='_blank' className={styles.mainContainerDownloadBtn}>
                   {item.downloadAllLogos}
-                </a>
+                </a> */}
+                <Download />
+
                 <div className={styles.mainContainerDownloadText}>{item.downloadAs}PDF, EPS & SVG</div>
               </div>
             </div>
