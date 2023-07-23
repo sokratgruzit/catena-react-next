4906f3acf0b0444e282f545d98127ba1c50a876b
NewGate <NewGate@newgate>
2023-07-23 08:18:22 -0400

    aa

--- components/home/careers-inner/components/submitAplication/SubmitAplication.js
+++ components/home/careers-inner/components/submitAplication/SubmitAplication.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import styles from './SubmitAplication.module.css'
+
+import styles from './SubmitAplication.module.css';
 // import { InputTest } from '@catena-network/catena-ui-module';
-// import { InputTest } from '@catena-network/catena-ui-module'
+// import { TableElement } from '@catena-network/catena-ui-module'
 // import { Input } from '@catena-network/catena-ui-module'
 
 const SubmitAplication = ({ title }) => {
@@ -9,7 +10,40 @@ const SubmitAplication = ({ title }) => {
     <div className='container_bordered'>
       <h2 className={styles.font__51}>{title}</h2>
       <div className='container_bordered-child'>
-
+        {/* <Input
+          type={"default"}
+          // value={value}
+          icon={true}
+          emptyFieldErr={true}
+          inputType={"text"}
+          placeholder={"Enter.."}
+          label={"FULL NAME"}
+          subLabel={""}
+          customStyles={{ width: "500px" }}
+        />
+        <Input
+          type={"default"}
+          icon={false}
+          label={"E-MAIL"}
+          subLabel={""}
+          placeholder={"Enter.."}
+          value={""} // value
+          customStyles={{ width: "500px" }}
+        />
+        <Input
+          type={"label-input-phone-number"}
+          label={"PHONE NUMBER"}
+          customStyles={{ width: "500px" }}
+        />
+        <Input
+          type={"textarea"}
+          label={"DESCRIBE ANY EXPERIENCE WITH CRYPTOCURRENCY"}
+          name={"textarea input"}
+          rows={10}
+          cols={20}
+          placeholder={"Enter.."}
+          resize={"both"}
+        /> */}
       </div>
     </div>
   );
