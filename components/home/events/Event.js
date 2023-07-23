1e3ee4a05d5954551334b7c02cc2f33bedab878d
entost <tsotnentost@gmail.com>
2023-07-23 19:40:01 +0400

    update

--- components/home/events/Event.js
+++ components/home/events/Event.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Title from "./components/Title";
-import Paragraph from "./components/Paragraph";
 import Boxs from "./components/Boxs";
-import JoinCommunity from "./components/JoinCommunity";
+// import JoinCommunity from "./components/JoinCommunity";
 
 const Event = () => {
     return (
         <div style={{ paddingTop: '200px', background: "#FFF3E5" }}>
-            <Title />
-            <Paragraph />
             <Boxs />
-            <JoinCommunity />
+            {/* <JoinCommunity /> */}
         </div>
     );
 };
