984aeb55d60be3848fe3503f88118a09a4ddd241
NewGate <NewGate@newgate>
2023-07-24 11:21:09 -0400

    all

--- components/home/careers-inner/components/submitAplication/SubmitAplication.js
+++ components/home/careers-inner/components/submitAplication/SubmitAplication.js
@@ -7,86 +7,96 @@ import styles from './SubmitAplication.module.css';
 
 const SubmitAplication = ({ title }) => {
   return (
-    <div className='container_bordered'>
-      <h2 className={styles.font__51}>{title}</h2>
-      <div className='container_bordered-child'>
-        <div className={styles.infoImport}>
-          <Input
-            type={'default'}
-            // value={value}
-            icon={true}
-            emptyFieldErr={true}
-            inputType={'text'}
-            placeholder={'Enter..'}
-            label={'FULL NAME'}
-            subLabel={''}
-            customStyles={{ width: '500px' }}
-          />
-          <Input
-            type={'default'}
-            icon={false}
-            label={'E-MAIL'}
-            subLabel={''}
-            placeholder={'Enter..'}
-            value={''} // value
-            customStyles={{ width: '500px' }}
-          />
-          <Input type={'label-input-phone-number'} label={'PHONE NUMBER'} customStyles={{ width: '500px' }} />
-          <Input
-            type={'textarea'}
-            label={'DESCRIBE ANY EXPERIENCE WITH CRYPTOCURRENCY'}
-            name={'textarea input'}
-            rows={10}
-            cols={20}
-            placeholder={'Enter..'}
-            resize={'both'}
-          />
+    <div className={styles.submitWrapper}>
+      <div className='container_bordered'>
+        <h2 className={styles.font__51}>{title}</h2>
+        <div className='container_bordered-child'>
+          <div className={styles.infoImport}>
+            <Input
+              type={'default'}
+              // value={value}
+              icon={true}
+              emptyFieldErr={true}
+              inputType={'text'}
+              placeholder={'Enter..'}
+              label={'FULL NAME'}
+              subLabel={''}
+            />
 
-          <Input
-            type={'textarea'}
-            label={'Languages you can speak/write at a Business level'}
-            name={'textarea input'}
-            rows={10}
-            cols={20}
-            placeholder={'Enter..'}
-            resize={'both'}
-          />
+            <Input
+              type={'default'}
+              icon={false}
+              label={'E-MAIL'}
+              subLabel={''}
+              placeholder={'Enter..'}
+              value={''} // value
+            />
 
-          <Input
-            type={'textarea'}
-            label={'Additional information'}
-            name={'textarea input'}
-            rows={10}
-            cols={20}
-            placeholder={'Enter..'}
-            resize={'both'}
-          />
+            <Input
+              type={'label-input-phone-number'}
+              label={'PHONE NUMBER'}
+            />
 
-          <Input
-            type={'default'}
-            // value={value}
-            icon={true}
-            emptyFieldErr={true}
-            inputType={'text'}
-            placeholder={'Enter..'}
-            label={'Github'}
-            subLabel={''}
-            customStyles={{ width: '500px' }}
-          />
+            <Input
+              type={'textarea'}
+              label={'DESCRIBE ANY EXPERIENCE WITH CRYPTOCURRENCY'}
+              name={'textarea input'}
+              rows={10}
+              cols={20}
+              placeholder={'Enter..'}
+              resize={'both'}
+            />
 
-          <Input
-            type={'default'}
-            // value={value}
-            icon={true}
-            emptyFieldErr={true}
-            inputType={'text'}
-            placeholder={'Enter..'}
-            label={'Linkedin'}
-            subLabel={''}
-            customStyles={{ width: '500px' }}
-          />
+            <Input
+              type={'textarea'}
+              label={'Languages you can speak/write at a Business level'}
+              name={'textarea input'}
+              rows={10}
+              cols={20}
+              placeholder={'Enter..'}
+              resize={'both'}
+            />
 
-          <Button label={'submit Now'} size={'btn-lg'} type={'btn-primary'} arrow={'arrow-right'} element={'button'} />
+            <Input
+              type={'textarea'}
+              label={'Additional information'}
+              name={'textarea input'}
+              rows={10}
+              cols={20}
+              placeholder={'Enter..'}
+              resize={'both'}
+            />
+
+            <Input
+              type={'default'}
+              // value={value}
+              icon={true}
+              emptyFieldErr={true}
+              inputType={'text'}
+              placeholder={'Enter..'}
+              label={'Github'}
+              subLabel={''}
+            />
+
+            <Input
+              type={'default'}
+              // value={value}
+              icon={true}
+              emptyFieldErr={true}
+              inputType={'text'}
+              placeholder={'Enter..'}
+              label={'Linkedin'}
+              subLabel={''}
+            />
+
+            <Button
+              label={'submit Now'}
+              size={'btn-lg'}
+              type={'btn-primary'}
+              arrow={'arrow-right'}
+              element={'button'}
+            />
+          </div>
         </div>
       </div>
     </div>
