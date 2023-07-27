4d7428beaa2e80adabb69aabf4704de8a0617c6e
LexaBog <lexabogit@gmail.com>
2023-07-27 18:05:06 +0400

    update

--- pages/home/earn/farms/auction/index.js
+++ pages/home/earn/farms/auction/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CommunityAuctions from '../../../../components/earn/communityAuctions/CommunityAuctions';
+import CommunityAuctions from '../../../../../components/earn/communityAuctions/CommunityAuctions';
 
 const index = () => {
   return <CommunityAuctions />;
