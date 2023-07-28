461f41a3122f9c434d1db5595b98e8f483f0ee0d
entost <tsotnentost@gmail.com>
2023-07-28 18:26:12 +0400

    last

--- components/home/press/Press.js
+++ components/home/press/Press.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import createAxiosInstance from '../../../pages/api/axios';
 
 import styles from './Press.module.css';
+import Years from './components/filterWithYears/Years';
 
 const Press = () => {
   const [activeYear, setActiveYear] = useState('');
@@ -90,38 +91,7 @@ const Press = () => {
           </div>
         </div>
       </div>
-      <div className={styles.infoContainer}>
-        <div className={styles.infContTitle}>
-          <h2 className='ttl font-40'>Publics by years</h2>
-        </div>
-        <div className={`${styles.yearsStats}`}>
-          <div
-            className={activeYear === '2023' ? styles.activeCont : styles.pasCont}
-            onClick={() => handleYearClick('2023')}
-          >
-            2023
-          </div>
-          <div
-            className={activeYear === '2020' ? styles.activeCont : styles.pasCont}
-            onClick={() => handleYearClick('2020')}
-          >
-            2022
-          </div>
-          <div
-            className={activeYear === '2019' ? styles.activeCont : styles.pasCont}
-            onClick={() => handleYearClick('2019')}
-          >
-            2021
-          </div>
-          <div
-            className={activeYear === '2018' ? styles.activeCont : styles.pasCont}
-            onClick={() => handleYearClick('2018')}
-          >
-            2020
-          </div>
-        </div>
-
-      </div>
+        <Years handleYearClick={handleYearClick} activeYear={activeYear}/>
       <div className={styles.statisticContainer}>
         {filterData ? filterData.map((item, index) => {
           return (
---
+++ components/home/press/components/filterWithYears/Years.js
@@ -0,0 +1,39 @@
+import styles from "../../Press.module.css"
+
+const Years = ({handleYearClick, activeYear}) => {
+    return (
+        <div className={styles.infoContainer}>
+            <div className={styles.infContTitle}>
+                <h2 className='ttl font-40'>Publics by years</h2>
+            </div>
+            <div className={`${styles.yearsStats}`}>
+                <div
+                    className={activeYear === '2023' ? styles.activeCont : styles.pasCont}
+                    onClick={() => handleYearClick('2023')}
+                >
+                    2023
+                </div>
+                <div
+                    className={activeYear === '2020' ? styles.activeCont : styles.pasCont}
+                    onClick={() => handleYearClick('2020')}
+                >
+                    2022
+                </div>
+                <div
+                    className={activeYear === '2019' ? styles.activeCont : styles.pasCont}
+                    onClick={() => handleYearClick('2019')}
+                >
+                    2021
+                </div>
+                <div
+                    className={activeYear === '2018' ? styles.activeCont : styles.pasCont}
+                    onClick={() => handleYearClick('2018')}
+                >
+                    2020
+                </div>
+            </div>
+      </div>
+    )
+}
+
+export default Years;
