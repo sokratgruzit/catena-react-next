2709391ed9d262cb20aa4accbf87f57ff80f53a0
whatever <eqiza99@gmail.com>
2023-07-30 22:29:08 +0400

    update

--- components/home/careers-inner/components/submitAplication/SubmitAplication.js
+++ components/home/careers-inner/components/submitAplication/SubmitAplication.js
@@ -1,7 +1,10 @@
-import { Input, Button } from '@catena-network/catena-ui-module';
-import React from 'react';
-import { Quiz } from '@catena-network/catena-ui-module';
 import { useState } from 'react';
+
+import axios from 'axios';
+
+import { Input, Button } from '@catena-network/catena-ui-module';
+import { Quiz } from '@catena-network/catena-ui-module';
+
 import styles from './SubmitAplication.module.css';
 
 const TEMPRORAYDATA = [
@@ -15,8 +18,8 @@ const TEMPRORAYDATA = [
   {
     question: "Do you require Visa sponsorship to work in your location?",
     answer: [
-      { option: "No" },
       { option: "Yes, sponsorship is required" },
+      { option: "No" },
     ]
   },
 
@@ -24,8 +27,22 @@ const TEMPRORAYDATA = [
 
 const SubmitAplication = ({ title }) => {
 
+  const [triggerValidation, setTriggerValidation] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
-
+  const [applicantData, setApplicantData] = useState({
+    full_name: "",
+    email: "",
+    phone_number: "",
+    experience_with_cryptocurrency: "",
+    languages_speak_write: "",
+    additional_information: "",
+    github: "",
+    linkedin: "",
+    cv: "",
+    job_id: "",
+    quiz_answers: selectedAnswers
+  });
 
   const handleOptionChange = (questionIndex, optionIndex) => {
     const updatedAnswers = [...selectedAnswers];
@@ -33,65 +50,104 @@ const SubmitAplication = ({ title }) => {
     setSelectedAnswers(updatedAnswers);
   };
 
-  const handlerSubmit = (e) => {
+  const handlerSubmit2 = (e) => {
     e.preventDefault();
 
     console.log(selectedAnswers);
   };
 
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    console.log(applicantData, 'data')
+    setIsLoading(true);
+    setTriggerValidation(true)
+
+    try {
+      await axios
+        .post(`${API_URL}/submit_application`, { applicantData })
+        .then((res) => {
+          console.log(res);
+          triggerValidation(false);
+          setIsLoading(false);
+        })
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={styles.submitWrapper}>
       <div className='container_bordered'>
-        <h2 className={styles.font__51}>{title}</h2>
+        <h2 className={styles.font__51}>{"title"}</h2>
         <div className='container_bordered-child'>
           <div className={styles.infoImport}>
             <Input
               type={"default"}
-              // value={value}
+              value={applicantData.full_name}
               icon={true}
-              emptyFieldErr={false}
+              emptyFieldErr={triggerValidation && applicantData.full_name === "" ? true : false}
               inputType={"text"}
               placeholder={"Enter.."}
               label={"FULL NAME"}
               subLabel={""}
-              onChange={(e) => { console.log(e.target.value); }}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  full_name: e.target.value,
+                });
+              }}
             />
-
             <Input
               type={"default"}
               icon={false}
+              value={applicantData.email}
               label={'E-MAIL'}
-              editable={true}
-              // value={""}
               subLabel={""}
+              emptyFieldErr={triggerValidation && applicantData.email === "" ? true : false}
               placeholder={'Enter..'}
-              onChange={(e) => { console.log(e.target.value); }}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  email: e.target.value,
+                });
+              }}
             />
-
             <Input
               type={'label-input-phone-number'}
               label={'PHONE NUMBER'}
-              // value={''}
-              onChange={() => { console.log("number"); }}
+              value={applicantData.phone_number}
+              emptyFieldErr={triggerValidation && applicantData.phone_number === "" ? true : false}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  phone_number: e,
+                });
+              }}
             />
-
             <Input
               type={'textarea'}
               label={'Describe any experience with cryptocurrency'}
               name={'textarea input'}
+              value={applicantData.experience_with_cryptocurrency}
+              emptyFieldErr={triggerValidation && applicantData.experience_with_cryptocurrency === "" ? true : false}
               rows={10}
               cols={20}
               placeholder={'Enter..'}
               resize={'none'}
               customStyles={{ resize: "none", height: "126px" }}
-              onChange={(e) => { console.log(e.target.value); }}
-            />
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  experience_with_cryptocurrency: e.target.value,
+                });
+              }} />
 
             <Quiz
               selectedAnswers={selectedAnswers}
               handleOptionChange={handleOptionChange}
               quizData={TEMPRORAYDATA}
-              // handlerSubmit={submitHendler}
+              // emptyFieldErr={triggerValidation && applicantData.full_name === "" ? true : false}
               customStyles={{
                 width: "100%",
                 backgroundColor: "#F3E4D2",
@@ -99,70 +155,95 @@ const SubmitAplication = ({ title }) => {
                 color: "#162029"
               }}
             />
-
             <Input
               type={'textarea'}
               label={'Languages you can speak/write at a Business level'}
               name={'textarea input'}
+              value={applicantData.languages_speak_write}
+              emptyFieldErr={triggerValidation && applicantData.languages_speak_write === "" ? true : false}
               rows={10}
               cols={20}
               placeholder={'Enter..'}
               resize={'none'}
               customStyles={{ resize: "none", height: "126px" }}
-              onChange={(e) => { console.log(e.target.value); }}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  languages_speak_write: e.target.value,
+                });
+              }}
             />
-
             <Input
               type={'textarea'}
               label={'Additional information'}
               name={'textarea input'}
+              value={applicantData.additional_information}
+              emptyFieldErr={triggerValidation && applicantData.additional_information === "" ? true : false}
               rows={10}
               cols={20}
               placeholder={'Enter..'}
               resize={'none'}
               customStyles={{ resize: "none", height: "126px" }}
-              onChange={(e) => { console.log(e.target.value); }}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  additional_information: e.target.value,
+                });
+              }}
             />
-
             <Input
               type={'default'}
-              // value={value}
+              value={applicantData.github}
+              emptyFieldErr={triggerValidation && applicantData.github === "" ? true : false}
               icon={true}
-              emptyFieldErr={false}
               inputType={'text'}
               placeholder={'Enter..'}
               label={'Github'}
               subLabel={''}
-              onChange={(e) => { console.log(e.target.value); }}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  github: e.target.value,
+                });
+              }}
             />
-
             <Input
               type={'default'}
-              // value={value}
+              value={applicantData.linkedin}
               icon={true}
-              emptyFieldErr={false}
+              emptyFieldErr={triggerValidation && applicantData.linkedin === "" ? true : false}
               inputType={'text'}
               placeholder={'Enter..'}
               label={'Linkedin'}
               subLabel={''}
-              onChange={(e) => { console.log(e.target.value); }}
+              onChange={(e) => {
+                setApplicantData({
+                  ...applicantData,
+                  linkedin: e.target.value,
+                });
+              }}
             />
-
             <Input
               type={"label-input-upload-document"}
-              emptyFieldErr={true}
+              // emptyFieldErr={triggerValidation && applicantData.cv === "" ? true : false}
               htmlFor={""}
+              emptyFieldErr={false}
+              onChange={(i) => {
+                setApplicantData({
+                  ...applicantData,
+                  cv: i,
+                });
+              }}
               customStyles={{ width: "fit-content" }}
             />
-
             <Button
-              label={'submit Now'}
+              label={!isLoading ? 'submit Now' : "Loading..."}
               size={'btn-lg'}
               type={'btn-primary'}
               arrow={'arrow-right'}
               element={'button'}
-              disabled={false}
-              onClick={() => { console.log('rame'); }}
+              disabled={isLoading}
+              onClick={handlerSubmit}
             />
           </div>
         </div>
