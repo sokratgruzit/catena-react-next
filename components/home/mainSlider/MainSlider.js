c2d8d79d9355ac69c07d1e44c752d69969e3fa7f
sergo93@gmail.com <sergo93@gmail.com>
2023-08-06 17:37:47 +0400

    last

--- components/home/mainSlider/MainSlider.js
+++ components/home/mainSlider/MainSlider.js
@@ -10,7 +10,6 @@ import styles from './MainSlider.module.css';
 import Image from "next/image";
 
 const MainSlider = ({ trans }) => {
-    console.log(trans);
   const [levels, setLevels] = useState([]);
   const [activeSlide, setActiveSlide] = useState(0);
   const activeLang = useSelector(state => state.settings.activeLang);
@@ -19,14 +18,14 @@ const MainSlider = ({ trans }) => {
   useEffect(() => {
       dispatch({
           type: "SET_MICHROSCHEME_ARRAY",
-          microschemeArray: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48]
+          microschemeArray: [1,2,3,4,5,6,7,8,9,10,11,12]
       });
       // setLevels([7]);
       setActiveSlide(1)
   },[])
   let microSchemes = [
-      [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48],
-      [1,3,4,6,10,11,12],
+      [1,2,3,4,5,6,7,8,9,10,11,12],
+      [1],
       [2,3,6,7,8,12],
       [1,4,5,6,7,11,12],
       [1,2,3,7,8]
