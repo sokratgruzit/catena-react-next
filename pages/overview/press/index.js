8821f0f7e1f0e67ef5d1fd815f978ce8aae71005
aLLod1n <bekalichava22@gmail.com>
2023-08-07 19:50:46 +0400

    update

--- pages/overview/press/index.js
+++ pages/overview/press/index.js
@@ -2,26 +2,29 @@ import createAxiosInstance from '../../../pages/api/axios';
 import Press from '../../../components/home/press/Press';
 
 export async function getServerSideProps({ query }) {
-  const currentPage = parseInt(query.page, 10) || 1;
+  const currentPage = query.page || 1;
   const limit = 4;
+  
 
   const axios = createAxiosInstance();
   const response = await axios.get(`${process.env.NEXT_PUBLIC_URL}/press/get-all-press`, {
-    params: { type: "main", page: currentPage, limit: limit },
+    params: { page: currentPage, limit: limit },
+    
   });
-
   const pressData = response.data.press;
+  const current = response.data.currentPage
+  const totalPages = response.data.totalPages;
 
   return {
     props: {
       press: pressData,
-      currentPage,
-      totalCount: 10,
+      currentPage: current,
+      totalCount: totalPages,
     },
   };
 }
 
-const Index = ({ press, currentPage = 1, totalCount = 10 }) => {
+const Index = ({ press, currentPage = 1, totalCount }) => {
 
   return (
     <div style={{ paddingTop: '150px' }}>
