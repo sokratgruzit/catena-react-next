78ac324ce2c1ba612c05efe69162f99d8601290b
David Tavadze <tavadzed@gmail.com>
2023-08-08 12:52:19 +0400

    last

--- components/home/mainSlider/MainSlider.js
+++ components/home/mainSlider/MainSlider.js
@@ -11,6 +11,7 @@ import Image from "next/image";
 import TaskSvg from '../../svg/mainSlider/TaskSvg';
 import SolutionSvg from '../../svg/mainSlider/SolutionSvg';
 
+//Prettify this file
 const MainSlider = ({ trans }) => {
     const [levels, setLevels] = useState([]);
     const [activeSlide, setActiveSlide] = useState(0);
