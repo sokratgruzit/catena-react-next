8248fbe68e2b42069fc5a0c2c65b351a53243792
alex <pipiaalex7@gmail.com>
2023-08-09 17:01:43 +0400

    rame

--- components/home/events/Events.js
+++ components/home/events/Events.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Boxs from './components/Boxs';
 
 const Events = ({ events }) => {
-  return <Boxs events={events} /> /* Pass the event data as a prop to the Boxs component */
+  return (
+    <div className='pT-180' >
+      <Boxs events={events} /> {/* Pass the event data as a prop to the Boxs component */}
+    </div>
+  );
 };
 
 export default Events;
