14543e2ce45261cf2d1ef28f3a2f258ae5c0bd55
LexaBog <lexabogit@gmail.com>
2023-08-17 13:48:49 +0400

    last

--- pages/overview/contribute/index.js
+++ pages/overview/contribute/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Contribute from '../../../components/home/contrubute/Contrubute';
 
-const index = () => {
-  return <div>Contribute</div>;
+const idex = () => {
+  return (
+    <div style={{ paddingTop: '150px', backgroundColor: '#fff2e4' }}>
+      <Contribute />
+    </div>
+  );
 };
 
-export default index;
+export default idex;
