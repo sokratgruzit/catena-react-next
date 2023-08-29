2eb3150aac2365438c97b519b05e8017a267c13a
alex <pipiaalex7@gmail.com>
2023-08-29 16:24:16 +0400

    ra

--- pages/overview/confirmation/index.js
+++ pages/overview/confirmation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Confirmation from '../../../components/home/confirmationYourPayment/Confirmation'
+import Confirmation from '../../../components/home/ConfirmationYourPayment/Confirmation';
 
 const index = () => {
   return <Confirmation />;
