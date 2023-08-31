4570e9e77f5531a731b99d3842655830238ce2fa
David Tavadze <tavadzed@gmail.com>
2023-08-31 16:39:44 +0400

    l

--- pages/overview/info/index.js
+++ pages/overview/info/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InfoOverview from '../../../components/home/info/infoOverview/InfoOverview.js';
 
 const index = () => {
+  //comment
   return <InfoOverview />;
 };
 
