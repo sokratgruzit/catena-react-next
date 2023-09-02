eb5d7eca7c384cca87bf24486b94804da273f90f
alex <pipiaalex7@gmail.com>
2023-09-02 05:48:46 +0400

    ram

--- components/info/components/infoCharts/InfoCharts.js
+++ components/info/components/infoCharts/InfoCharts.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, Line, CartesianGrid, LineChart } from 'recharts';
+import { useMobileWidth } from '../../../../hooks/useMobileWidth';
 
 import styles from './InfoCharts.module.css';
 const data = Array(1000).fill({ name: 'B', value: 20 },);
@@ -11,28 +12,24 @@ const data = Array(1000).fill({ name: 'B', value: 20 },);
 // ];
 
 const InfoCharts = () => {
+  const { width } = useMobileWidth();
+  console.log(width)
   return (
     <>
       <h2 className='ttl font-40'>CATENA Network Info & Analytics</h2>
       <div className={styles.Container}>
-        <div className={styles.Chart}>
-          <div style={{ width: '100%', height: '100%' }}>
-            <BarChart width={400} height={300} data={data}>
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="value" fill="#8884d8" />
-            </BarChart>
-          </div>
+        <div className={styles.Chart} style={{padding: '10px'}}>
+          <LineChart width={width / 2.3} height={511} data={data}>
+            <XAxis dataKey="value" tick={{ fill: 'balck' }} />
+            <Tooltip contentStyle={{ backgroundColor: 'lightgray', color: 'black' }} />
+            <Line type="monotone" dataKey="value" stroke="#8884d8" />
+          </LineChart>
         </div>
         <div className={styles.Chart}>
-          <BarChart width={100} height={100} data={data}>
-            <XAxis dataKey="name" tick={{ fill: 'balck' }} />
-            {/* <YAxis /> */}
+          <BarChart width={width / 2.2} height={511} data={data}>
+            <XAxis dataKey="value" tick={{ fill: 'balck' }} />
             <Tooltip contentStyle={{ backgroundColor: 'lightgray', color: 'black' }} />
-            {/* <Legend iconType="circle" iconSize={10} /> */}
-            <Bar dataKey="value" fill="gray" />
+            <Bar type="monotone" dataKey="value" fill="gray" />
           </BarChart>
         </div>
       </div>
