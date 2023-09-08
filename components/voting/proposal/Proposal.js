87cac993bb405c4791d12a8dceff5d7d17352974
NTsloth <sloth2003nt@gmail.com>
2023-09-08 22:22:20 +0400

    proposal component

--- components/voting/proposal/Proposal.js
+++ components/voting/proposal/Proposal.js
@@ -1,192 +1,128 @@
-// import React, { useState } from 'react';
-// import { useRouter } from 'next/router';
-// import DOMPurify from 'dompurify';
-// import ChoiceItemRow from '../components/choiceItem/ChoiceItemRow';
-// import ChoiceItemResult from '../components/choiceItem/ChoiceItemResult';
-// import CountdownItem from '../components/countdownItem/CountdownItem';
-// import VotingData from '../components/votingData/VotingData';
-// import Button from '../../UI/button/Button';
+import React, { useState, useEffect } from 'react';
+import { Input, Button } from '@catena-network/catena-ui-module';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css';
+import styles from './Proposal.module.css';
 
-// import { CloseTag, CommunityTag, CoreTagLight, OpenSvg, SmlArrowSvg, SoonTag, VoteNowTag } from '../../svg/index';
+export default function Proposal() {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [time, setTime] = useState('');
+  const { quill, quillRef } = useQuill();
+  const [value, setValue] = useState();
 
-// import { useConnect } from '../../../../catena-react-admin-module/src/hooks/useConnect';
-// import { useWindowDimension } from '../../../hooks/useWindowDimension';
+  React.useEffect(() => {
+    if (quill) {
+      quill.on('text-change', () => {
+        console.log(quillRef.current.firstChild.innerHTML);
+        setValue(quillRef.current.firstChild.innerHTML);
+      });
+    }
+  }, [quill]);
 
-// import styles from './Proposal.module.css';
+  console.log(value, 'this is quill editor');
 
-// const votingData = [
-//   {
-//     id: 0,
-//     title: 'Stop Minting more CATENA',
-//     img: '/images/voting/proposalImg.png',
-//     date: 'Ends Mar 6th, 2022 11:15',
-//     location: 'Community',
-//     activeStatus: 'Vote Now',
-//     choices: [
-//       {
-//         value: 'yes we are happy',
-//         id: 0,
-//         totalVotes: 1368085.43,
-//         percentage: 90.03,
-//       },
-//       {
-//         value: 'no we are not happy at all',
-//         id: 1,
-//         totalVotes: 70901.43,
-//         percentage: 41.03,
-//       },
-//     ],
-//     countdownTo: Date.now() + 100000000,
-//     mainText:
-//       '<h2>CATENA Network Welcomes Tiny World ($TINC) to Syrup Pool!</h2><p><br></p><p>Dear CAKE holders, we’re proud to announce a new Syrup Pool with Tiny World! Tiny World is the 1st idle-style GameFi project where everything is available to trade. Play enjoyable, idle-style games with a global community of gamers and earn a passive income in your spare time.</p><p><br></p><h2>The Syrup Pool:</h2><p>Stake CAKE tokens to earn TINC tokens!</p><ul><li>Total Tokens: 404,100 TINC</li><li>Distribution duration: 30 days</li><li>Start time: Approx. 11AM UTC on April 7th 2022</li><li>Finish time: Approx. 11AM UTC on May 7th 2022</li><li>Token rewards per block: 0.4677 TINC</li></ul><h2>Token rewards per block: 0.4677 TINC</h2><p><br></p><p>100 CATENA for the first 57,600 blocks (approx. 48 hours) after the Syrup Pool is launched, then unlimited CATENA staking after that.</p><p>The cap is to give everyone an equal opportunity to farm rewards at a high APY at the start of the farm.</p><p><br></p><p><br></p><h2>How do you stake CATENA to earn $TINC rewards?</h2><p>Follow this guide to staking in Syrup Pools:</p><p><a href="https://docs.pancakeswap.finance/products/syrup-pool/syrup-pool-guide" rel="noopener noreferrer" target="_blank">https://docs.pancakeswap.finance/products/syrup-pool/syrup-pool-guide</a></p><p><br></p><p><br></p><h2><br></h2>',
-//   },
-// ];
+  const handleInputChange = event => {
+    const inputTime = event.target.value;
 
-// const Proposal = () => {
-//   const router = useRouter();
-//   const { id } = router.query;
-//   const { isActive, handleWalletModal } = useConnect();
-//   const [width, height] = useWindowDimension();
+    setTime(inputTime);
+  };
 
-//   const [votingItem, setVotingItem] = useState(votingData[0]);
-//   const [activeVoteWindow, setActiveVoteWindow] = useState('cast');
+  const handleStartDateChange = date => {
+    setStartDate(date);
+  };
 
-//   const [votingChoice, setVotingChoice] = useState(null);
+  const handleEndDateChange = date => {
+    setEndDate(date);
+  };
 
-//   const handleFormSubmit = e => {
-//     e.preventDefault();
-//     setActiveVoteWindow('vote');
-//     console.log('hi');
-//   };
+  const handleInputChanges = event => {
+    console.log('hi');
+  };
+  return (
+    <div className={`${styles.main} `}>
+      <div className={styles.proposalWrapper}>
+        <div className={`${styles.proposalIntro} container`}>
+          <h1>Make a Proposal</h1>
+          <p className='font_30'>
+            Scroll presently pads accost twilight byron spasms bate bacchus cottage stainless boils freely lacinia. Fire
+            aid easiest accumsan scroll pealed, broke, boils depart.
+          </p>
+        </div>
+        <div className={`${styles.bottomBox}`}>
+          <form className={styles.form}>
+            <Input
+              className={styles.llll}
+              type={'default'}
+              icon={false}
+              label={'title'}
+              subLabel={''}
+              placeholder={'Enter'}
+              name='text'
+              onChange={handleInputChanges}
+            />
+            <Input
+              className={styles.llll}
+              type={'default'}
+              icon={false}
+              label={'Content'}
+              subLabel={'Tip: Write in Markdown!'}
+              placeholder={'Enter'}
+              name='text'
+              onChange={handleInputChanges}
+            />
+            <div className={styles.content}>
+              <label className={styles.label}>Content</label>
+              <label className={styles.subLabel}>Tip: Write in Markdown!</label>
+              <div className={styles.contentTextarea}>
+                <div ref={quillRef} />
+              </div>
+            </div>
 
-//   return (
-//     <>
-//       <div className={`container ${styles.wrapper}`}>
-//         <main className={styles.ProposalInfo}>
-//           <section className={styles.proposalSection}>
-//             <nav onClick={() => router.push('/voting')} className={styles.backBtn}>
-//               <SmlArrowSvg className={styles.arrowSvg} />
-//               <p className={styles.blueHover}>Back</p>
-//             </nav>
-//             <CountdownItem votingTo={votingItem.countdownTo} />
-//             <h1 className={styles.title}>{votingItem.title}</h1>
-//             <div
-//               className={styles.mainBody}
-//               dangerouslySetInnerHTML={{
-//                 __html: DOMPurify.sanitize(votingItem.mainText),
-//               }}
-//             ></div>
-//           </section>
-//           <div className={styles.detailsWrapper}>
-//             <div className={styles.details}>
-//               <div className={styles.gradient}></div>
-//               <div className={styles.detailsCont}>
-//                 <p>Details</p>
-//                 <div className={styles.tagWrapper}>
-//                   {votingItem.activeStatus === 'Close' && <CloseTag />}
-//                   {votingItem.activeStatus === 'Vote Now' && <VoteNowTag />}
-//                   {votingItem.activeStatus === 'Soon' && <SoonTag />}
-//                   {votingItem.location === 'Community' && <CommunityTag />}
-//                   {votingItem.location === 'CATENA' && <CoreTagLight />}
-//                 </div>
-//               </div>
-//               <div className={styles.item}>
-//                 <p>Identifier</p>
-//                 <div className={styles.itemInner}>
-//                   <a href='##'>
-//                     QmWVfYm3
-//                     <OpenSvg />
-//                   </a>
-//                 </div>
-//               </div>
-//               <div className={styles.item}>
-//                 <p>Creator</p>
-//                 <div className={styles.itemInner}>
-//                   <a href='##'>
-//                     0xC7...061b <OpenSvg />
-//                   </a>
-//                 </div>
-//               </div>
-//               <div className={`${styles.item} ${styles.borderBottom}`}>
-//                 <p>Snapshot</p>
-//                 <div className={styles.itemInner}>
-//                   <a href='##'>
-//                     16704554 <OpenSvg />
-//                   </a>
-//                 </div>
-//               </div>
-//               {width > 1023 && (
-//                 <>
-//                   {votingItem.activeStatus === 'Close' && <CloseTag />}
-//                   {votingItem.activeStatus === 'Vote Now' && <VoteNowTag />}
-//                   {votingItem.activeStatus === 'Soon' && <SoonTag />}
-//                 </>
-//               )}
-//               <div className={styles.item}>
-//                 <p>Start Date</p>
-//                 <p className={styles.dateData}>2022-04-06 14:00</p>
-//               </div>
-//               <div className={styles.item}>
-//                 <p>End Date</p>
-//                 <p className={styles.dateData}>2022-04-07 14:00</p>
-//               </div>
-//             </div>
-//             {activeVoteWindow === 'cast' ? (
-//               <div className={`${styles.vote}`}>
-//                 <h3>Cast Your Vote</h3>
-//                 <span className={styles.border}></span>
-//                 <form onSubmit={handleFormSubmit} className={styles.form}>
-//                   {votingItem?.choices?.map((choice, index) => (
-//                     <ChoiceItemRow key={index} index={index} choice={choice} setVotingChoice={setVotingChoice} />
-//                   ))}
-//                   {isActive ? (
-//                     <button className={`btn btnBlue ${styles.voteButton}`} disabled={votingChoice === null}>
-//                       Vote
-//                     </button>
-//                   ) : (
-//                     <Button
-//                       title={'Connect Wallet'}
-//                       onClick={() => {
-//                         handleWalletModal(true);
-//                       }}
-//                       type={'blue'}
-//                       className={styles.connectWallet}
-//                     />
-//                   )}
+            <Input
+              className={styles.llll}
+              type={'default'}
+              icon={false}
+              label={'Choices'}
+              subLabel={''}
+              placeholder={'Enter'}
+              name='text'
+              onChange={handleInputChanges}
+            />
 
-//                   <p className={styles.seeMore} onClick={() => setActiveVoteWindow('see result')}>
-//                     See Result
-//                   </p>
-//                 </form>
-//               </div>
-//             ) : (
-//               <div className={`${styles.result} ${activeVoteWindow && styles.resultActive}`}>
-//                 {activeVoteWindow === 'see result' ? (
-//                   <div className={styles.resultHeader} onClick={() => setActiveVoteWindow('cast')}>
-//                     <SmlArrowSvg />
-//                     <h3>Results</h3>
-//                   </div>
-//                 ) : (
-//                   <h3>Current Results</h3>
-//                 )}
-
-//                 <span className={styles.border}></span>
-//                 <div className={styles.votes}>
-//                   {votingItem?.choices?.map((choice, index) => (
-//                     <ChoiceItemResult key={index} choice={choice} />
-//                   ))}
-//                   <p className={styles.votedChoice}>
-//                     you have voted for <span>{votingChoice.value}</span>
-//                   </p>
-//                 </div>
-//               </div>
-//             )}
-//           </div>
-//         </main>
-//         <VotingData />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Proposal;
+            <div className={styles.inputContainer}>
+              <label className={styles.label}>Date</label>
+              <label className={styles.subLabel}>Start Date</label>
+              <DatePicker
+                selected={startDate}
+                onChange={handleStartDateChange}
+                dateFormat='MM/dd/yyyy'
+                className={styles.datePicker}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label className={styles.subLabel}>End Date</label>
+              <DatePicker
+                selected={endDate}
+                onChange={handleEndDateChange}
+                dateFormat='MM/dd/yyyy'
+                className={styles.datePicker}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label className={styles.label}>Time</label>
+              <label className={styles.subLabel}>Start Time</label>
+              <input placeholder='00:00' type='time' required className={styles.input} />
+            </div>
+            <div className={styles.inputContainer}>
+              <label className={styles.subLabel}>End Time</label>
+              <input placeholder='00:00' type='time' required className={styles.input} />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
