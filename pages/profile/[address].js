7e17655ce16fdfae9b23d7ce84228438e9832878
David Tavadze <tavadzed@gmail.com>
2023-09-10 13:36:10 +0400

    last

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
 import ProtectedRoute from '../../components/layout/ProtectedRoute'
 import Profile from '../../components/nfts/components/Profile';
 
 const index = () => {
     const router = useRouter();
-    const { address } = router.query;
+    // const { address } = router.query;
+    const address = useSelector(state => state.connect.account);
 
     return (
         <ProtectedRoute>
