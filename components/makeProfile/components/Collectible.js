ef80b9686e39be7a1613ef023d9c93be6c70493f
alex <pipiaalex7@gmail.com>
2023-09-17 05:37:51 +0400

    ram

--- components/makeProfile/components/Collectible.js
+++ components/makeProfile/components/Collectible.js
@@ -35,7 +35,13 @@ const Collectible = ({ bodyElement, buttonLabel, disable, onClick, title, text, 
                     />
                     <div className={styles.flex}>
                         <p style={{ color: '#389A5F' }}>{helpText}</p>
-                        <a href={`https://etherscan.io/tx/${transactionHash}`}>{transactionHash}</a>
+                        {transactionHash && <a className={styles.link} href={`https://etherscan.io/tx/${transactionHash}`}>
+                            View on BscScan: {transactionHash}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M18.5396 15.1046C18.674 14.9703 18.7588 14.7864 18.7588 14.5743L18.7588 5.99003C18.7576 5.7916 18.6783 5.60164 18.538 5.46133C18.3977 5.32101 18.2077 5.24167 18.0093 5.2405L9.425 5.2405C9.01488 5.2405 8.67547 5.57991 8.67547 5.99003C8.67547 6.40015 9.01488 6.73956 9.425 6.73956L17.2597 6.73956L17.2597 14.5743C17.2597 14.9844 17.5992 15.3238 18.0093 15.3238C18.2143 15.3309 18.4053 15.239 18.5396 15.1046Z" fill="#162029" />
+                                <path d="M6.5184 18.5424L18.419 6.64177C18.7089 6.35186 18.7089 5.87103 18.419 5.58111C18.1291 5.2912 17.6483 5.2912 17.3583 5.58111L5.45773 17.4817C5.16782 17.7716 5.16782 18.2525 5.45773 18.5424C5.74765 18.8323 6.22848 18.8323 6.5184 18.5424Z" fill="#162029" />
+                            </svg>
+                        </a>}
                     </div>
                 </div>
             </div>
